d89de971729ac4b9ef6a0089ea8a7eddbb76b66b
WebluxDigi <159778328+TeamWebLux@users.noreply.github.com>
2024-05-25 16:32:03 +0530

    Update slider.jsx

--- js/game/slider.jsx
+++ js/game/slider.jsx
@@ -111,7 +111,7 @@ const Slider = () => {
       <div className="image-item-container">
         <a href={game.url} className="image-item">
           <div className="overlay-image">
-            <img src="/gangster_assets/cards/card.png" alt="Overlay" />
+            <img src="/gangster_assets/cards/test.png" alt="Overlay" />
             <img src={game.src} alt={`img-${game.id}`} className="game-image" />
           </div>
         </a>
