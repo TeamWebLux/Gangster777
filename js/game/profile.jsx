28b471585844cb5228672f3726f864b6b433ea67
Hiren Maurya <webluxdigi@gmail.com>
2024-05-27 09:54:17 +0000

    Auto-update from server

--- js/game/profile.jsx
+++ js/game/profile.jsx
@@ -1,1 +1,19 @@
-class Profile{}
+import React from 'react';
+
+const ProfileImage = ({ src, height, width }) => {
+  const imageStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: height,
+    width: width
+  };
+
+  return (
+    <div>
+      <img src={src} alt="Profile" style={imageStyle} />
+    </div>
+  );
+};
+
+export default ProfileImage;
