4aa65c5733ab0c4af48cb8e197d428434aaa0b3d
Hiren Maurya <webluxdigi@gmail.com>
2024-05-27 10:02:49 +0000

    Auto-update from server

--- js/game/profile.jsx
+++ js/game/profile.jsx
@@ -5,8 +5,8 @@ const ProfileImage = () => {
     position: 'absolute',
     top: 0,
     left: 0,
-    height: '200px',
-    width: '400px'  
+    height: '160px',
+    width: 'auto'  
   };
 
   return (
