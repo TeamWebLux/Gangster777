d3d3719b188ff8d7904eb5b6ee670b53c6abb3c8
Hiren Maurya <webluxdigi@gmail.com>
2024-05-30 13:07:54 +0000

    Auto-update from server

--- js/Login.jsx
+++ js/Login.jsx
@@ -147,7 +147,7 @@ class LoginWin extends Component {
     src="/gangster_assets/login-btn.png"
     alt="Login"
     onClick={this.handleSubmit}
-    style={{ width: '150px', height: '50px' }} // Set your desired width and height here
+    style={{ width: '150px', height: '50px' }}
 />
 
 
