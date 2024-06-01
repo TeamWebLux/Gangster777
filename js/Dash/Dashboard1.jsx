53ea5356c1067f561073a3416a6e456734051f65
Hiren Maurya <webluxdigi@gmail.com>
2024-06-01 13:49:04 +0000

    Auto-update from server

--- js/Dash/Dashboard1.jsx
+++ js/Dash/Dashboard1.jsx
@@ -1,17 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './css/LoginWin.css';
 
-const Dashboard1 = () => {
-  return (
-    <div>
-      <h1>Dashboard 1</h1>
-      <p>Welcome to Dashboard 1. Here you can find some specific information and actions related to this section.</p>
-      <ul>
-        <li>Feature 1</li>
-        <li>Feature 2</li>
-        <li>Feature 3</li>
-      </ul>
-    </div>
-  );
-};
+class BackgroundAnimation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            backgroundImageIndex: 0,
+            backgroundImages: [
+                '/gangster_assets/login_page_bg.png',
+                '/gangster_assets/login_page_bg_car_light.png',
+                '/gangster_assets/login_page_bg_home_light.png',
+                '/gangster_assets/login_page_bg_light.png'
+            ],
+            imagesLoaded: false,
+        };
+        this._isMounted = false;
+    }
 
-export default Dashboard1;
+    componentDidMount() {
+        this._isMounted = true;
+        this.loadImages();
+        this.backgroundInterval = setInterval(this.changeBackground, 2500); // Change background every 2.5 seconds
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+        clearInterval(this.backgroundInterval);
+    }
+
+    loadImages = () => {
+        const { backgroundImages } = this.state;
+        let imagesLoadedCount = 0;
+        backgroundImages.forEach(src => {
+            const img = new Image();
+            img.onload = () => {
+                if (this._isMounted) {
+                    imagesLoadedCount++;
+                    if (imagesLoadedCount === backgroundImages.length) {
+                        this.setState({ imagesLoaded: true });
+                    }
+                }
+            };
+            img.src = src;
+        });
+    }
+
+    changeBackground = () => {
+        const { backgroundImages, backgroundImageIndex } = this.state;
+        const newIndex = (backgroundImageIndex + 1) % backgroundImages.length;
+        this.setState({ backgroundImageIndex: newIndex });
+    }
+
+    render() {
+        const { backgroundImageIndex, backgroundImages, imagesLoaded } = this.state;
+        const backgroundImage = backgroundImages[backgroundImageIndex];
+        return (
+            <div>
+                {!imagesLoaded && (
+                    <div className="preloader">
+                        <div className="preloader-text">Loading...</div>
+                    </div>
+                )}
+                {imagesLoaded && (
+                    <div className="background-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
+export default BackgroundAnimation;
