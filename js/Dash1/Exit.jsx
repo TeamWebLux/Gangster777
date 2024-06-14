6227980fe16ec018a6efe86abc9b4bdaf0aba6ec
Hiren Maurya <webluxdigi@gmail.com>
2024-06-14 15:06:51 +0000

    Auto-update from server

--- js/Dash1/Exit.jsx
+++ js/Dash1/Exit.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+// Styled component
+const ExitContainer = styled.div`
+    position: absolute;
+    top: 10px; /* Adjust as needed */
+    right: 10px; /* Adjust as needed */
+    z-index: 1000; /* Ensure it is on top of other elements */
+`;
+
+const ExitImage = styled.img`
+    width: 50px; /* Adjust size as needed */
+    height: 50px; /* Adjust size as needed */
+    cursor: pointer;
+`;
 
 const Exit = () => {
     return (
-       <div>
-        Hello
-       </div>
+        <ExitContainer>
+            <ExitImage src="/gangster_assets/dash1/profile/exit.png" alt="Exit" />
+        </ExitContainer>
     );
 };
 
